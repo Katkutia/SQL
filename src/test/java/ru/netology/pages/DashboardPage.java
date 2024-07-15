20c2e1585fc88e73ecaeaa3fc0960c942a88c3f2
Katerina Platonova <platonovihome@gmail.com>
2024-07-15 13:39:12 +0300

    cheange src/test/java/ru/netology/test/AuthTest.java

--- src/test/java/ru/netology/pages/DashboardPage.java
+++ src/test/java/ru/netology/pages/DashboardPage.java
@@ -1,17 +1,16 @@
-package pages;
+ package ru.netology.pages;
+ import com.codeborne.selenide.SelenideElement;
 
-import com.codeborne.selenide.SelenideElement;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
+ import static com.codeborne.selenide.Condition.text;
+ import static com.codeborne.selenide.Condition.visible;
+ import static com.codeborne.selenide.Selenide.$;
 
 
-public class DashboardPage {
-    private final SelenideElement heading = $("[data-test-id=dashboard]");
+ public class DashboardPage {
+     private final SelenideElement heading = $("[data-test-id=dashboard]");
 
-    // метод для проверки того что вы попали на страницу Dashboard
-    public DashboardPage() {
-        heading.shouldHave(text("Личный кабинет")).shouldBe(visible);
-    }
-}
+     // метод для проверки того что вы попали на страницу Dashboard
+     public DashboardPage() {
+         heading.shouldHave(text("Личный кабинет")).shouldBe(visible);
+     }
+ }
